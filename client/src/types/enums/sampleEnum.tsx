7c46dba904dba48065e6767637d4f470423b2907
Traven Gonzales <travengonzales@gmail.com>
2023-10-04 16:57:17 -0700

    feat(added-types-directories): Added directories for types, enums, and interfaces

---
+++ client/src/types/enums/sampleEnum.tsx
@@ -0,0 +1,8 @@
+// Sample enum file to maintain enum directory in project structure
+enum SampleEnum  {
+    RED = "RED",
+    BLUE = "BLUE",
+    YELLOW = "YELLOW"
+}
+
+export { SampleEnum }
---
+++ client/src/types/types/sampleType.tsx
@@ -0,0 +1,4 @@
+// Sample type file to maintain types folder in project directory
+type SampleType = 10 | 15 | 30;
+
+export type { SampleType }
