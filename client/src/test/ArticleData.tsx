7eb2463e54beb82d1439346d84f3174ad4525fc3
Traven Gonzales <travengonzales@gmail.com>
2023-10-09 14:26:58 -0700

    chore(article-test-image): Added test image to go with article test data

--- client/src/test/ArticleData.tsx
+++ client/src/test/ArticleData.tsx
@@ -1,35 +1,37 @@
 import { Article } from "../types/interfaces/Article"
 
+import testImage from "assets/article-test-image.jpg"
+
 const ArticleTestData : Article[] = [
     {
         id: "1",
-        imgSrc: "",
+        imgSrc: testImage,
         heading: "test article 1",
         subHeading: "this is the 1st test article",
         tags: ["Breaking News", "Local News"]
     },
     {
         id: "2",
-        imgSrc: "",
+        imgSrc: testImage,
         heading: "test article 2",
         subHeading: "this is the 2nd test article",
-        tags: ["Government"]
+        tags: ["Government", "Local News"]
     },
     {
         id: "3",
-        imgSrc: "",
+        imgSrc: testImage,
         heading: "test article 3",
         subHeading: "this is the 3rd test article",
         tags: ["High School Sports"]
     },
     {
         id: "4",
-        imgSrc: "",
+        imgSrc: testImage,
         heading: "test article 4",
         subHeading: "this is the 4th test article",
         tags: ["Crime"]
     }
-];
+]
 
 export default ArticleTestData
 
