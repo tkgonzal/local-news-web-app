f5ad5c9f902f8df1e8fd59e16aa7d565f44d1196
ivan piceno <ivan.piceno171@gmail.com>
2023-10-09 15:27:09 -0700

    simple layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
 import { Route, Routes } from "react-router-dom"
 
 import "./App.css"
+import Navbar from "./components/Navbar";
 
 function App() : JSX.Element {
+
+  const navLinks = [
+    { name: 'Home', url: '/' },
+    { name: 'Business panel', url: '/panel' },
+    { name: 'Staff', url: '/staff' },
+    { name: 'Login', url: '/login' },
+    { name: 'Subscribe', url: '/subscribe' },
+  ];
+
   return (
     <>
+      <Navbar title="Local News App" links={navLinks}/>
+
       <Routes>
         <Route path="/" element={<h1>home page</h1>}/>
-        <Route path="/news" element={<h1>news page</h1>}/>
+        <Route path="/panel" element={<h1>Business Panel</h1>}/>
+        <Route path="/staff" element={<h1>Staff</h1>}/>
+        <Route path="/login" element={<h1>Login</h1>}/>
+        <Route path="/subscribe" element={<h1>Subscribe</h1>}/>
       </Routes>
     </>
   )
