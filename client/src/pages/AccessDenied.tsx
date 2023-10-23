9e9ab248b99654323d018be5e92428df9b18ad8f
Traven Gonzales <travengonzales@gmail.com>
2023-10-23 15:58:35 -0700

    feat(access-denied-page): Created AccessDenied page component

--- client/src/pages/AccessDenied.tsx
+++ client/src/pages/AccessDenied.tsx
@@ -1,10 +1,9 @@
-import "./AccessDenied.css"
-
+// Page to display when the user has insufficient permissions to access a page
 const AccessDenied: React.FC = () => {
     return (
         <main className="access-denied--container">
             <h1>Access Denied</h1>
-            <p>User must log in or have sufficient page</p>
+            <p>User must log in or have sufficient permissions to access page</p>
         </main>
     )
 }
