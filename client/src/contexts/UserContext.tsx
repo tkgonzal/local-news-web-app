24794a54d0f09632ab354a427a162aa34720ce60
Traven Gonzales <travengonzales@gmail.com>
2023-10-26 13:48:44 -0700

    chore(user-context): Removed extraneous semicolons from UserContext

--- client/src/contexts/UserContext.tsx
+++ client/src/contexts/UserContext.tsx
@@ -1,29 +1,29 @@
-import { ReactNode, createContext, useContext, useState } from "react";
-import { User } from "../types/interfaces/User";
+import { ReactNode, createContext, useContext, useState } from "react"
+import { User } from "../types/interfaces/User"
 
 interface UserContextType {
-    user: User | null;
-    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+    user: User | null
+    setUser: React.Dispatch<React.SetStateAction<User | null>>
 };
 
-const UserContext = createContext<UserContextType | undefined>(undefined);
+const UserContext = createContext<UserContextType | undefined>(undefined)
 
 export function useUserContext() {
-    const context = useContext(UserContext);
+    const context = useContext(UserContext)
     if (!context) {
-        throw new Error('useUserContext must be used within a UserProvider');
+        throw new Error('useUserContext must be used within a UserProvider')
     }
-    return context;
+    return context
 };
 
 interface UserProviderProps {
-    children: ReactNode;
+    children: ReactNode
 };
 
 export function UserProvider({ children }: UserProviderProps) {
-    const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User | null>(null)
 
-    console.log('context user: ', user);
+    console.log('context user: ', user)
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
