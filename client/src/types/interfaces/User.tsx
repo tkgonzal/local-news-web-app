53766db2d63c8c7b7bc280c7962c99604f0e5f18
Traven Gonzales <travengonzales@gmail.com>
2023-11-07 16:51:10 -0800

    refactor(user-type): Made User type in client a modified import from the server's definition

--- client/src/types/interfaces/User.tsx
+++ client/src/types/interfaces/User.tsx
@@ -1,26 +1,8 @@
 // Interface currently only accounds for members needed for BusinessPanel
-import { Name } from "./Name"
-import { UserAccountType } from "../types/UserAccountType"
-import Permission from "../enums/Permission"
+import { User as backendUser } from "../../../../server/models/User" 
 
-interface User {
-    _id: string | null
-    name?: Name
-    email: string
-    phone?: string
-    accType: UserAccountType
-    // For non-Business User accounts, indicates the _id of 
-    // a Business User account they're associated with
-    businessId?: string
-    businessName?: string
-    businessWebsite?: string
-    articlePermissions?: Permission
-    userPermissions?: Permission
-    hasDisabledLogin?: boolean
-    // For Business Accounts, Indicates whether or not the account should receive
-    // an email about new comment notifcation
-    receivesCommentNotifications?: boolean
-    notes?: string
+interface User extends Omit<backendUser, "_id"> {
+    _id: string
 }
 
 export type { User }
