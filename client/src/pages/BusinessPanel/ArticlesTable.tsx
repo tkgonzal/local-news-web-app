71be7fc9f953f10f85a76235babae96053810361
Traven Gonzales <travengonzales@gmail.com>
2023-11-08 14:56:55 -0800

    feat(articles-table): Set up logic to pull articles for a business on ArticlesTable

--- client/src/pages/BusinessPanel/ArticlesTable.tsx
+++ client/src/pages/BusinessPanel/ArticlesTable.tsx
@@ -1,14 +1,35 @@
+import { useState, useEffect } from "react"
 import { NavigateFunction, useNavigate } from "react-router-dom"
+
+import usePanelTableState from "../../hooks/usePanelTableState"
 
 import BusinessPanelPage from "./BusinessPanelPage"
 import PanelTable from "../../components/BusinessPanel/PanelTable"
 
-import ArticleTestData from "../../test/ArticleData"
+import { Article } from "../../types/interfaces/Article"
+
+// import ArticleTestData from "../../test/ArticleData"
 
 // Page used for dashboard, displaying a business's articles and options
 // to edit/delete them or make new ones
 const ArticlesTable: React.FC = () => {
+    const { user, location, shouldRefresh, setShouldRefresh } = usePanelTableState()
+    const [businessArticles, setBusinessArticles] = useState<Article[]>([])
     const formNavigate: NavigateFunction = useNavigate()
+
+    // Side Effects
+    // Effect to retrieve the articles for a business whenever the ArticlesTable
+    // must refresh or is loaded
+    useEffect(() => {
+        // Helper function for getting articles associated with a business
+        const getArticlesForBusiness = async () => {
+            if (user) {
+                const businessId = user.businessId || user._id
+
+                // const businessArticlesResponse
+            }
+        }
+    }, [user, location, shouldRefresh])
 
     // Event handlers
     // Event handler to navigate the user to a new article form
@@ -24,7 +45,7 @@ const ArticlesTable: React.FC = () => {
                     <button onClick={makeNewArticle}>+ New Article</button>
                 </div>
 
-                <PanelTable tableType="Article" tableContents={ArticleTestData}/>
+                <PanelTable tableType="Article" tableContents={businessArticles}/>
             </>
         </BusinessPanelPage>
     )
