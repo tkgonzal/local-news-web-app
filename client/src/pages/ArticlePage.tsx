17eb79bf81ba4d584c93b16b0f465fbf089ffff0
Traven Gonzales <travengonzales@gmail.com>
2023-11-14 13:52:16 -0800

    refactor(article-page): Changed process of rendering string bodies for an article

--- client/src/pages/ArticlePage.tsx
+++ client/src/pages/ArticlePage.tsx
@@ -61,7 +61,9 @@ const ArticlePage: React.FC = () => {
     }
 
     const mainImage = articleObj.images.length ? articleObj.images[0] : defaultMainArticleImage
-    const body = typeof articleObj.body == "string" ? <p>{articleObj.body}</p> : articleObj.body.map((text, index)=>(<p key={index}>{text}</p>))
+    const body = typeof articleObj.body == "string" ? 
+        <div dangerouslySetInnerHTML={{__html: articleObj.body}}></div> : 
+        articleObj.body.map((text, index)=>(<p key={index}>{text}</p>))
 
     const articleThumbnails = [<ArticleThumbnail key={articleObj._id?.toString()} className="main-article" article={articleObj}/>].concat(
         recommendedArticles.map((article)=><ArticleThumbnail key={article._id?.toString()} article={article}/>)
