44ed69ba35afa9e0d20cc9cc64e78e07860c31c9
Jordan Dinis <jordan.dinis.cohort231@gmail.com>
2023-11-21 00:25:58 -0800

    feat(subcategories): Added new fetches and logic to make subcategory carousels use the proper articles

--- client/src/pages/NewsPage.tsx
+++ client/src/pages/NewsPage.tsx
@@ -11,14 +11,11 @@ import { useLocation } from "react-router-dom"
 
 const BASE_API_URL = import.meta.env.VITE_SERVER_URL
 
-// interface Props {
-//     articles: Article[]
-// }
 
 // Page component for the news page of the app which displays all the news subcategories
 const NewsPage: React.FC = () => {
     const location = useLocation()
-    const [newsArticles,setArticles] = useState<Article[]>([])
+    const [newsArticles,setArticles] = useState<Article[][]>([])
     useEffect(() => {
         if(location.pathname === "/news"){
             fetchArticles()
@@ -27,28 +24,59 @@ const NewsPage: React.FC = () => {
 
     const fetchArticles = async () => {
         try {
-            const headlineResponse = await axios.get(
+            const breakingResponse = await axios.get(
                 `${BASE_API_URL}/api/articles?tag=Breaking%20News`
             )
-            setArticles(headlineResponse.data)
+            const localResponse = await axios.get(
+                `${BASE_API_URL}/api/articles?tag=Local%20News`
+            )
+            const crimeResponse = await axios.get(
+                `${BASE_API_URL}/api/articles?tag=Crime`
+            )
+            const governmentResponse = await axios.get(
+                `${BASE_API_URL}/api/articles?tag=Government`
+            )
+            const educationResponse = await axios.get(
+                `${BASE_API_URL}/api/articles?tag=Education`
+            )
+            setArticles([breakingResponse.data,localResponse.data,crimeResponse.data,governmentResponse.data,educationResponse.data])
         } catch (error) {
             console.log("An error occurred while retrieving Articles")
             console.log(error)
         }
     }
 
-    
+    //Breaking News
+    const breakingArticles: Article[] = newsArticles.length? newsArticles[0] : []
     const mainArticleThumbnail: JSX.Element = 
-        newsArticles.length ? <ArticleThumbnail className="main-article" article={newsArticles[0]}/> : <></>
+        breakingArticles.length ? <ArticleThumbnail className="main-article" article={newsArticles[0][0]}/> : <></>
 
-    const secondaryArticleThumbnails: JSX.Element[] = newsArticles.slice(1,5).map(
+    const secondaryArticleThumbnails: JSX.Element[] = breakingArticles.slice(1,5).map(
         (article: Article) => 
             <ArticleThumbnail key={article._id} article={article} />
     )
-    const featuredArticleThumbnails: JSX.Element[] = newsArticles.slice(5).map(
+    const featuredArticleThumbnails: JSX.Element[] = breakingArticles.slice(5).map(
         (article: Article) => 
             <ArticleThumbnail key={article._id} article={article} />
     )
+
+    //Subcategory carousels
+    const localArticles: JSX.Element[] = newsArticles.length? newsArticles[1].map(
+        (article: Article) => 
+            <ArticleThumbnail key={article._id} article={article} />
+    ) : []
+    const crimeArticles: JSX.Element[] = newsArticles.length? newsArticles[2].map(
+        (article: Article) => 
+            <ArticleThumbnail key={article._id} article={article} />
+    ) : []
+    const governmentArticles: JSX.Element[] = newsArticles.length? newsArticles[3].map(
+        (article: Article) => 
+            <ArticleThumbnail key={article._id} article={article} />
+    ) : []
+    const educationArticles: JSX.Element[] = newsArticles.length? newsArticles[4].map(
+        (article: Article) => 
+            <ArticleThumbnail key={article._id} article={article} />
+    ) : []
     console.log(newsArticles)
 
     return (
@@ -78,28 +106,28 @@ const NewsPage: React.FC = () => {
             <div className="subpage--article-carousel-container">
                 {
                     featuredArticleThumbnails.length &&
-                    <ArticleCarousel articleThumbnails={featuredArticleThumbnails}/>
+                    <ArticleCarousel articleThumbnails={localArticles}/>
                 }  
             </div>
             <h2 className="subpage--article-carousel-header">Crime</h2>
             <div className="subpage--article-carousel-container">
                 {
                     featuredArticleThumbnails.length &&
-                    <ArticleCarousel articleThumbnails={featuredArticleThumbnails}/>
+                    <ArticleCarousel articleThumbnails={crimeArticles}/>
                 }
             </div>
             <h2 className="subpage--article-carousel-header">Government</h2>
             <div className="subpage--article-carousel-container">
                 {
                     featuredArticleThumbnails.length &&
-                    <ArticleCarousel articleThumbnails={featuredArticleThumbnails}/>
+                    <ArticleCarousel articleThumbnails={governmentArticles}/>
                 }
             </div>
             <h2 className="subpage--article-carousel-header">Education</h2>
             <div className="subpage--article-carousel-container">
                 {
                     featuredArticleThumbnails.length &&
-                    <ArticleCarousel articleThumbnails={featuredArticleThumbnails}/>
+                    <ArticleCarousel articleThumbnails={educationArticles}/>
                 }
             </div>
 
