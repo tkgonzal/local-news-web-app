9ae98466aa9bdc0b76ae7797eb14cff967ddc029
Traven Gonzales <travengonzales@gmail.com>
2023-11-28 13:37:15 -0800

    feat(subscriptions-route): Added endpoint and logic to create a new Subscription to add to the DB

--- server/models/Subscription.ts
+++ server/models/Subscription.ts
@@ -2,6 +2,10 @@ import { Collection } from "mongodb";
 import { connectToDatabase } from "../config/db";
 import { Subscription } from "../types/interfaces/Subscription";
 
+/**
+ * @returns {Promise<Collection<Subscription>>} A collection of all the
+ * Subscriptions in the DB
+ */
 async function getSubscriptionCollection(): Promise<Collection<Subscription>> {
     const db = await connectToDatabase();
     
@@ -12,10 +16,34 @@ async function getSubscriptionCollection(): Promise<Collection<Subscription>> {
     return db.collection<Subscription>("Subscription");
 };
 
+/**
+ * @returns {Promise<Subscription[]>} The array of all subscriptions in the db
+ */
 async function getSubscriptions(): Promise<Subscription[]> {
     const subscriptionsCollection = await getSubscriptionCollection();
     const subscriptions = await subscriptionsCollection.find({});
     return subscriptions.toArray();
 }
 
-export { getSubscriptions }
+/**
+ * @param subscription A subscription to add to the db
+ * @returns {Promise<Subscription | null>} The subscription as added to the db
+ */
+async function createSubscription(subscription: Subscription): 
+    Promise<Subscription | null> {
+        const subscriptions = await getSubscriptionCollection();
+        const inserted = await subscriptions.insertOne(subscription);
+
+        if (inserted.insertedId) {
+            const insertedSubscription = {
+                ...subscription,
+                _id: inserted.insertedId
+            } as Subscription;
+
+            return insertedSubscription;
+        } else {
+            throw new Error("Subscription not added");
+        }
+}
+
+export { getSubscriptions, createSubscription }
--- server/routes/subscription.ts
+++ server/routes/subscription.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
-import { getSubscriptions } from "../models/Subscription";
+import { getSubscriptions, createSubscription } from "../models/Subscription";
 
 import dotenv from "dotenv";
+import { Subscription } from "../types/interfaces/Subscription";
 dotenv.config();
 
 const router = express.Router();
@@ -20,6 +21,33 @@ router.get("/", async (req, res) => {
         console.log("Error retrieving subscriptions: ", error);
         res.status(500).json({
             message: "Internal Server Error Occurred Retrieving Subscriptions",
+            error
+        });
+    }
+});
+
+// Adds a subscription to the DB
+router.post("/new", async (req, res) => {
+    try {
+        const { subscriptionData } = req.body;
+
+        const subscription: Subscription = {
+            email: subscriptionData.email,
+            phone: subscriptionData.phone,
+            frequency: subscriptionData.frequency,
+            subscribedForAll: subscriptionData.subscribedForAll,
+            subscriptionTypes: subscriptionData.subscriptionTypes
+        };
+
+        await createSubscription(subscription);
+
+        res.status(200).json({
+            message: "New subscription added"
+        });
+    } catch (error: any) {
+        console.log("Error adding subscription: ", error);
+        res.status(500).json({
+            message: "Internal Server Error Occurred Adding Subscription",
             error
         });
     }
