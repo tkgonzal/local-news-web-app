1ef3a9195a3329bfdf2eeadb04c00b62e2596104
Traven Gonzales <travengonzales@gmail.com>
2023-12-01 16:22:11 -0800

    feat(subscriptions): Made and fleshed out file for subscriptions service

--- cron/src/services/subscriptions.ts
+++ cron/src/services/subscriptions.ts
@@ -1,16 +1,29 @@
 import axios from "axios";
 import dotenv from "dotenv";
 
+import { SubscriptionFrequency } from "../types/types/SubscriptionFrequencies";
+
 // Setup
 dotenv.config();
 
 // Utility Functions
-const getSubscriptions = () => {
+// Retrieves the subscriptions in the DB that match the given subscription 
+// frequency
+const getSubscriptions = async (frequency: SubscriptionFrequency) => {
+    try {
 
+    } catch (error: any) {
+        console.log("Error occurred while gathering subscriptions");
+        throw error;
+    }
 }
 
 const sendOutSubscriptionNewsletters = () => {
+    try {
 
+    } catch (error: any) {
+        console.log("Error occurred while sending out subscriptions", error);
+    }
 }
 
-export { getSubscriptions };
+export { sendOutSubscriptionNewsletters };
