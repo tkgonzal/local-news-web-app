5d91ede16eed68a16590709cdf94d846c678115e
liam-sh <liamsh@gmail.com>
2023-12-18 14:19:49 -0800

    feat: disable new comments

--- client/src/pages/ArticlePage/NewCommentField.tsx
+++ client/src/pages/ArticlePage/NewCommentField.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import './NewCommentField.css'
 
-export default function NewCommentField(props:{postComment: (comment:string)=>Promise<void>}) {
+export default function NewCommentField(props:{postComment: (comment:string)=>Promise<void>, disabled?:boolean}) {
     const [comment, setComment] = useState<string>("")
 
     function onSubmit(e: React.FormEvent) {
@@ -18,11 +18,17 @@ export default function NewCommentField(props:{postComment: (comment:string)=>Pr
         <textarea
             onChange={(e)=>setComment(e.target.value)}
             value={comment}
-            placeholder="Share your thoughts..."
+            placeholder={props.disabled?"Sign in to leave a comment.":"Share your thoughts..."}
             className="comment-form-text"
+            disabled={props.disabled}
         >
         </textarea>
-        {comment?<input type="submit" value="Post" className="submit-button"/>:<></>}
+        {comment?<input
+            type="submit"
+            value="Post"
+            className="submit-button"
+            disabled={props.disabled}
+        />:<></>}
     </form>
     )
 }
