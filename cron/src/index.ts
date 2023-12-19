7cffeb88d2b97a16eec258bbac843c440dd2d4f1
Traven Gonzales <travengonzales@gmail.com>
2023-12-18 18:32:35 -0800

    feat(notification): Added notification service to daily cron job

--- cron/src/index.ts
+++ cron/src/index.ts
@@ -3,18 +3,20 @@ import { sendOutCommentNotifications } from "./services/notifications.js";
 import cron from "node-cron";
 
 (async () => {
-    await sendOutCommentNotifications();
-    // // Runs Every Hour
-    // cron.schedule("0 * * * * ", () => sendOutSubscriptionNewsletters("Hourly"));
-    // // Runs Every Hour At Midnight
-    // cron.schedule("0 0 * * * ", () => sendOutSubscriptionNewsletters("Daily"));
-    // // Runs Every Monday At Midnight
-    // cron.schedule("0 0 * * 1 ", () => sendOutSubscriptionNewsletters("Weekly"));
-    // // Runs Every 1st and 15th of the Month at Midnight
-    // cron.schedule(
-    //     "0 0 1,15 * * ", 
-    //     () => sendOutSubscriptionNewsletters("Biweekly")
-    // );
-    // // Runs Every 1st of the Month At Midnight
-    // cron.schedule("0 0 1 * * ", () => sendOutSubscriptionNewsletters("Weekly"));
+    // Runs Every Hour
+    cron.schedule("0 * * * * ", () => sendOutSubscriptionNewsletters("Hourly"));
+    // Runs Every Day At Midnight
+    cron.schedule("0 0 * * * ", () => {
+        sendOutCommentNotifications();
+        sendOutSubscriptionNewsletters("Daily");
+    });
+    // Runs Every Monday At Midnight
+    cron.schedule("0 0 * * 1 ", () => sendOutSubscriptionNewsletters("Weekly"));
+    // Runs Every 1st and 15th of the Month at Midnight
+    cron.schedule(
+        "0 0 1,15 * * ", 
+        () => sendOutSubscriptionNewsletters("Biweekly")
+    );
+    // Runs Every 1st of the Month At Midnight
+    cron.schedule("0 0 1 * * ", () => sendOutSubscriptionNewsletters("Weekly"));
 })();
